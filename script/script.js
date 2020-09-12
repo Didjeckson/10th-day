02cbbb4465f042fc175dc296c5cbf0041e425e79
Di-djeckson <didjeckson@gmail.com>
2020-09-12 17:25:42 +0300

    second version of first task

--- script/script.js
+++ script/script.js
@@ -6,9 +6,20 @@ const bookList = document.querySelector('.books'),
 console.log(bookList);
 console.log(books);
 
+/*
+first decission
 bookList.append(books[1]);
 bookList.append(books[0]);
 bookList.append(books[4]);
 bookList.append(books[3]);
 bookList.append(books[5]);
 bookList.append(books[2]);
+
+*/
+
+bookList.prepend(books[2]);
+bookList.prepend(books[5]);
+bookList.prepend(books[3]);
+bookList.prepend(books[4]);
+bookList.prepend(books[0]);
+bookList.prepend(books[1]);
